f40013ffd3adc591e2073b8a19e92cf1932a5303
Livingstone Fultang <livingstonefultang@gmail.com>
2014-10-16 20:19:31 +0100

    Calendar app for Budkit. Shows how to load additional routes on registration of the service provider

---
+++ src/Provider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Calendar;
+
+use Budkit\Application\Support\Service;
+use Budkit\Dependency\Container;
+use Budkit\Filesystem\File;
+
+class Provider implements Service
+{
+
+    protected $application;
+
+    public function __construct(Container $application)
+    {
+        $this->application = $application;
+    }
+
+    public function onRegister()
+    {
+        $application = $this->application;
+        //Can Include Routes; using $this->application->route->add() or simply \Route::add().
+        $application->route
+            //->setResourceCallable("App\\Calendar\\Calendar")
+            ->attachResource("/calendar", "App\\Calendar\\Calendar"); //collection of events
+    }
+
+    public function definition()
+    {
+        return array("app.register" => "onRegister");
+    }
+
+
+}
